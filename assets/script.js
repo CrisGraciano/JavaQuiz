2d7b1c278e9cce3a5e0c8d0f9be8f30f8a04da59
CrisGraciano <cristobalgraciano98@gmail.com>
2022-08-10 00:14:58 -0500

    html and css done, psudo code for java

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// quiz needs to be timed
+// quiz will end when timer runs out
+
+
+function startQuiz() {
+// once start button is pressed, quiz will start
+// quiz starts by showing window with question and answer
+}
+
+function nextQuestion() {
+    // will move to next question if question is answered or button next is pressed
+
+}
+
+function selectAnswer() {
+    //will show correct or wrong answer chosen
+    //will move to next question once answer is selected
+    // if wrong answer is selected, time will be taken off
+}
